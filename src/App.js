36e5f089b8c67f4107335c38d3766f663d38716f
BINTDB <binitdb@gmail.com>
2022-04-19 10:03:24 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+function App() {
+  const [people, setpeople] = useState(data)
+
+  return (
+    <main>
+      <section className='container'>
+        <h3>{people.length} Birthday today</h3>
+       <List people ={people}/>
+       <button onClick={() =>setpeople([])}>Clear Me</button>
+
+      </section>
+    </main>
+  )
+  
+}
+
+export default App;
